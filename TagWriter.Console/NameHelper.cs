36ab4cb38c9fb89c52eb1ce74a908ffcc39a3fcd
Aleksandr Zhedik <Aleksandr Zhedik>
2016-01-17 03:15:44 +0300

    Implement 'Check', 'Fix' and 'Write' commands.

--- TagWriter.Console/NameHelper.cs
+++ TagWriter.Console/NameHelper.cs
@@ -1,15 +1,78 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace TagWriter.Console
 {
     public static class NameHelper
     {
         private const string PATH_PATTERN = @"^.*\\(?<artist>.+)\\\[(?<year>\d{4})\] (?<album>.+)\\(?<no>\d{2})\. (((?<track>.+) \(feat\. (?<artists>.+)\))|(?<track>.+))\.mp3$";
+
+        private const string INVALID_PATH_PATTERN_01 = @"^(?<folder>.*)\\(?<artist>.+)\\\[(?<year>\d{4})\] (?<album>.+)\\(?<no>\d{2}) (?<track>.+)\.mp3$";
+        private const string INVALID_PATH_PATTERN_02 = @"^(?<folder>.*)\\(?<artist>.+)\\\[(?<year>\d{4})\] (?<album>.+)\\(?<no>\d{2})\.(?<track>.+)\.mp3$";
+        private const string INVALID_PATH_PATTERN_03 = @"^(?<folder>.*)\\(?<artist>.+)\\\[(?<year>\d{4})\] (?<album>.+)\\(?<no>\d{2})-(?<track>.+)\.mp3$";
+        private const string INVALID_PATH_PATTERN_04 = @"^(?<folder>.*)\\(?<artist>.+)\\\[(?<year>\d{4})\] (?<album>.+)\\(?<no>\d{2}) - (?<track>.+)\.mp3$";
+        private const string INVALID_PATH_PATTERN_05 = @"^(?<folder>.*)\\(?<artist>.+)\\\[(?<year>\d{4})\] (?<album>.+)\\(?<no>\d{2})-(?<track>.+)\.Mp3$";
+
+        private const string INVALID_PATH_PATTERN_06 = @"^(?<folder>.*)\\(?<artist>.+)\\\((?<year>\d{4})\) (?<album>.+)\\(?<no>\d{2}) - (?<track>.+)\.mp3$";
+        private const string INVALID_PATH_PATTERN_07 = @"^(?<folder>.*)\\(?<artist>.+)\\\d{2}-(?<album>.+) \((?<year>\d{4})\)\\(?<no>\d{2})\. (?<track>.+)\.mp3$";
+        private const string INVALID_PATH_PATTERN_08 = @"^(?<folder>.*)\\(?<artist>.+)\\(?<album>.+) \((?<year>\d{4})\)\\(?<no>\d{2})\. (?<track>.+)\.mp3$";
+
         private static readonly Regex _pathRegex;
+        private static readonly List<Regex> _invalidPathRegexes;
 
         static NameHelper()
         {
             _pathRegex = new Regex(PATH_PATTERN);
+
+            _invalidPathRegexes = new List<Regex>
+            {
+                new Regex(INVALID_PATH_PATTERN_01),
+                new Regex(INVALID_PATH_PATTERN_02),
+                new Regex(INVALID_PATH_PATTERN_03),
+                new Regex(INVALID_PATH_PATTERN_04),
+                new Regex(INVALID_PATH_PATTERN_05),
+                new Regex(INVALID_PATH_PATTERN_06),
+                new Regex(INVALID_PATH_PATTERN_07),
+                new Regex(INVALID_PATH_PATTERN_07)
+            };
+        }
+
+        public static bool IsFormatValid(this string path)
+        {
+            return _pathRegex.IsMatch(path);
+        }
+
+        public static bool TryParsePath(this string path, out TrackInfo trackInfo)
+        {
+            try
+            {
+                foreach (var regex in _invalidPathRegexes)
+                {
+                    var match = regex.Match(path);
+                    if (match.Success)
+                    {
+                        trackInfo = new TrackInfo
+                        {
+                            Folder = match.Groups["folder"].Value,
+                            Artist = match.Groups["artist"].Value,
+                            Year = match.Groups["year"].Value,
+                            Album = match.Groups["album"].Value,
+                            Number = match.Groups["no"].Value,
+                            Name = match.Groups["track"].Value
+                        };
+
+                        return true;
+                    }
+                }
+
+                trackInfo = null;
+                return false;
+            }
+            catch
+            {
+                trackInfo = null;
+                return false;
+            }
         }
 
         public static string ExtractArtistName(this string path)
@@ -59,6 +122,16 @@ namespace TagWriter.Console
                 return artistsString.Split(',');
             }
             return null;
+        }
+
+        public class TrackInfo
+        {
+            public string Folder { get; set; }
+            public string Artist { get; set; }
+            public string Year { get; set; }
+            public string Album { get; set; }
+            public string Number { get; set; }
+            public string Name { get; set; }
         }
     }
 }
--- TagWriter.Console/Program.cs
+++ TagWriter.Console/Program.cs
@@ -10,31 +10,28 @@ namespace TagWriter.Console
 {
     class Program
     {
-        private const string ROOT_DIRECTORY = @"D:\Music";
-
         static void Main(string[] args)
         {
             System.Console.OutputEncoding = Encoding.Unicode;
-            //Debug.Assert(false);
-            
 
-            var serialNumbers = "01016359482619120376802384023984023840283049238048";
+            var artists = Directory
+                .GetDirectories(Environment.CurrentDirectory);
 
-            var bytes = new byte[serialNumbers.Length * sizeof(char)];
-            Buffer.BlockCopy(serialNumbers.ToCharArray(), 0, bytes, 0, bytes.Length);
+            var command = args != null ? args[0] as string : string.Empty;
 
-            var sha = new SHA1CryptoServiceProvider();
-            var result = sha.ComputeHash(bytes);
+            if (string.Equals(command, "check", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Check(artists, args);
+            }
+            else if (string.Equals(command, "fix", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Fix(artists);
+            }
+            else if (string.Equals(command, "write", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Write(artists);
+            }
 
-            var chars = new char[result.Length / sizeof(char)];
-            Buffer.BlockCopy(result, 0, chars, 0, result.Length);
-
-            var output = new string(chars);
-            System.Console.WriteLine(serialNumbers.GetHashCode());
-
-            //System.Console.OutputEncoding = Encoding.Unicode;
-
-            //var artists = Directory.GetDirectories(ROOT_DIRECTORY);
             //foreach (var artist in artists)
             //{
             //    var albums = Directory.GetDirectories(artist);
@@ -43,43 +40,182 @@ namespace TagWriter.Console
             //        var tracks = Directory.GetFiles(album, "*.mp3");
             //        foreach (var track in tracks)
             //        {
-            //            using (var mp3file = new Mp3File(track, Mp3Permissions.ReadWrite))
-            //            {
-            //                var artistName = track.ExtractArtistName();
-            //                var contributingArtists = track.ExtractContributingArtists();
+            //            //using (var mp3file = new Mp3File(track, Mp3Permissions.ReadWrite))
+            //            //{
+            //            //    var artistName = track.ExtractArtistName();
+            //            //    var contributingArtists = track.ExtractContributingArtists();
 
-            //                var tag = mp3file.GetTag(Id3TagFamily.Version2x);
+                //            //    var tag = mp3file.GetTag(Id3TagFamily.Version2x);
 
-            //                tag.Band.EncodingType = Id3TextEncoding.Unicode;
-            //                tag.Band.Value = artistName;
+                //            //    tag.Band.EncodingType = Id3TextEncoding.Unicode;
+                //            //    tag.Band.Value = artistName;
 
-            //                tag.Album.EncodingType = Id3TextEncoding.Unicode;
-            //                tag.Album.Value = track.ExtractAlbumName();
+                //            //    tag.Album.EncodingType = Id3TextEncoding.Unicode;
+                //            //    tag.Album.Value = track.ExtractAlbumName();
 
-            //                tag.Year.EncodingType = Id3TextEncoding.Unicode;
-            //                tag.Year.Value = track.ExtractAlbumYear();
+                //            //    tag.Year.EncodingType = Id3TextEncoding.Unicode;
+                //            //    tag.Year.Value = track.ExtractAlbumYear();
 
-            //                tag.Title.EncodingType = Id3TextEncoding.Unicode;
-            //                tag.Title.Value = track.ExtractTrackName();
+                //            //    tag.Title.EncodingType = Id3TextEncoding.Unicode;
+                //            //    tag.Title.Value = track.ExtractTrackName();
 
-            //                tag.Track.EncodingType = Id3TextEncoding.Unicode;
-            //                tag.Track.Value = track.ExtractTrackNumber();
+                //            //    tag.Track.EncodingType = Id3TextEncoding.Unicode;
+                //            //    tag.Track.Value = track.ExtractTrackNumber();
 
-            //                tag.Artists.EncodingType = Id3TextEncoding.Unicode;
-            //                tag.Artists.Value.Clear();
-            //                tag.Artists.Value.Add(artistName);
+                //            //    tag.Artists.EncodingType = Id3TextEncoding.Unicode;
+                //            //    tag.Artists.Value.Clear();
+                //            //    tag.Artists.Value.Add(artistName);
 
-            //                if (contributingArtists != null)
-            //                {
-            //                    contributingArtists.ToList()
-            //                        .ForEach(x => tag.Artists.Value.Add(x));
-            //                }
+                //            //    if (contributingArtists != null)
+                //            //    {
+                //            //        contributingArtists.ToList()
+                //            //            .ForEach(x => tag.Artists.Value.Add(x));
+                //            //    }
 
-            //                mp3file.WriteTag(tag);
-            //            }
-            //        }
-            //    }
-            //}
+                //            //    mp3file.WriteTag(tag);
+                //            //}
+                //        }
+                //    }
+                //}
+        }
+
+        static void Check(string[] artists, string[] args)
+        {
+            var defaultColor = System.Console.ForegroundColor;
+            System.Console.ForegroundColor = ConsoleColor.Red;
+
+            var notificationMode = "track";
+
+            if (args.Length > 1)
+            {
+                notificationMode = args[1];
+            }
+
+            foreach (var artist in artists)
+            {
+                var albums = Directory.GetDirectories(artist);
+                foreach (var album in albums)
+                {
+                    var tracks = Directory.GetFiles(album, "*.mp3");
+                    foreach (var track in tracks)
+                    {
+                        if (!track.IsFormatValid())
+                        {
+                            if (ReferenceEquals(track, tracks.First()))
+                            {
+                                System.Console.WriteLine("Unexpected format: " + album);
+                            }
+                            //System.Console.WriteLine("Unexpected format: " + track);
+                        }
+                    }
+                }
+            }
+
+            System.Console.ForegroundColor = defaultColor;
+        }
+
+        static void Fix(string[] artists)
+        {
+            System.Console.WriteLine("Fix command started");
+            System.Console.ReadLine();
+
+            var defaultColor = System.Console.ForegroundColor;
+            System.Console.ForegroundColor = ConsoleColor.Green;
+
+            foreach (var artist in artists)
+            {
+                var albums = Directory.GetDirectories(artist);
+                foreach (var album in albums)
+                {
+                    var tracks = Directory.GetFiles(album, "*.mp3");
+                    foreach (var track in tracks)
+                    {
+                        if (!track.IsFormatValid())
+                        {
+                            var fileName = Path.GetFileName(track);
+                            var directoryPath = Path.GetDirectoryName(track);
+
+                            NameHelper.TrackInfo trackInfo;
+                            if (track.TryParsePath(out trackInfo))
+                            {
+                                var fixedAlbumName = string.Format("[{0}] {1}", trackInfo.Year, trackInfo.Album);
+                                var fixedFileName = string.Format("{0}. {1}.mp3", trackInfo.Number, trackInfo.Name);
+                                var fixedPath = Path.Combine(trackInfo.Folder, trackInfo.Artist, fixedAlbumName, fixedFileName);
+                                
+                                try
+                                {
+                                    Directory.CreateDirectory(Path.GetDirectoryName(fixedPath));
+                                    File.Move(track, fixedPath);
+
+                                    System.Console.WriteLine("File name fixed: {0} --> {1}", track, fixedPath);
+                                }
+                                catch
+                                {
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            System.Console.ForegroundColor = defaultColor;
+        }
+
+        static void Write(string[] artists)
+        {
+            var defaultColor = System.Console.ForegroundColor;
+            System.Console.ForegroundColor = ConsoleColor.Blue;
+            
+            foreach (var artist in artists)
+            {
+                var albums = Directory.GetDirectories(artist);
+                foreach (var album in albums)
+                {
+                    var tracks = Directory.GetFiles(album, "*.mp3");
+                    foreach (var track in tracks)
+                    {
+                        if (track.IsFormatValid())
+                        {
+                            try
+                            {
+                                using (var mp3file = new Mp3File(track, Mp3Permissions.ReadWrite))
+                                {
+                                    var artistName = track.ExtractArtistName();
+                                    var tag = mp3file.GetTag(Id3TagFamily.Version2x);
+
+                                    tag.Band.EncodingType = Id3TextEncoding.Unicode;
+                                    tag.Band.Value = artistName;
+
+                                    tag.Album.EncodingType = Id3TextEncoding.Unicode;
+                                    tag.Album.Value = track.ExtractAlbumName();
+
+                                    tag.Year.EncodingType = Id3TextEncoding.Unicode;
+                                    tag.Year.Value = track.ExtractAlbumYear();
+
+                                    tag.Title.EncodingType = Id3TextEncoding.Unicode;
+                                    tag.Title.Value = track.ExtractTrackName();
+
+                                    tag.Track.EncodingType = Id3TextEncoding.Unicode;
+                                    tag.Track.Value = track.ExtractTrackNumber();
+
+                                    tag.Artists.EncodingType = Id3TextEncoding.Unicode;
+                                    tag.Artists.Value.Clear();
+                                    tag.Artists.Value.Add(artistName);
+
+                                    mp3file.WriteTag(tag);
+                                }
+
+                                System.Console.WriteLine("Tags wited to: " + track);
+                            }
+                            catch
+                            {
+                            }
+                        }
+                    }
+                }
+            }
+
+            System.Console.ForegroundColor = defaultColor;
         }
     }
 }
